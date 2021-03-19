abad1ed8f4c896fedf7f0c38b8f4b5e413fd8563
weversonbdelima <weversonbdelima@gmail.com>
2021-03-19 00:24:40 -0300

    primeiro commit

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,62 +1,59 @@
 //definição
-var produtosComponent = Vue.extend({
+var postComponent = Vue.extend({
     template: `
-        <div id="produto"class="shadow p-3 mb-5 bg-white rounded">
-            <div id="card" class="card" style="width: 14rem;">
-            <img class="card-img-top" src="imagens/img01.jpg" alt="Card image cap" style="height: 16rem">
-                <div id="produto-corpo" class="card-body">
-                    <h5 class="card-title">
-                        <span>{{nome}}</span>
-                    </h5>
-                    <span class="card-text">Preço: {{preco}}</span>
-                    <label>Quantidade</label>
-                    <input type="number" @keyup.enter="getSubTotal" v-model="quantidade" />
-                    <span class="card-text" ref="subTotal">Subtotal: {{subTotal}}</span>
+        <div id="post"class="shadow p-3 mb-5 bg-white rounded">
+            <div id="card" class="" style="width: 14rem;">
+                <h3>{{title}}</h3>
+                </br>
+                <span>{{descrisao}}</span>
+                <div id="data">
+                    <span>{{data}}</span>
                 </div>
             </div>
         </div>
     `,
     props: {
-        nome: String,
-        image: String,
-        preco: Number,
+        title: String,
+        descrisao: String,
+        data: Date,
     },
-    data: function() {
-        return{
-            quantidade: 0,
-            subTotal: 0
-        }
-    },
-    methods:{
-        getSubTotal: function(){
-            this.subTotal = this.preco * this.quantidade;
-            this.$emit('child-subtotal', this.subTotal);
-        }
-    }
 })  
 
 //Registro
-Vue.component('produto', produtosComponent);
+Vue.component('post', postComponent);
+
+//Importando dados
+
 
 var app = new Vue({
     el: '#app',
     data: {
-      message: 'Hello Vue!',
-      produtos: [
-          {id: 0, nome: 'A', image: 'imagens/img01.jpg', preco: 10.00, quantidade: 0, subtotal: 0},
-          {id: 1, nome: 'B', image: '', preco: 20.00, quantidade: 0, subtotal: 0},
-          {id: 2, nome: 'C', image: '', preco: 30.00, quantidade: 0, subtotal: 0},
-          {id: 3, nome: 'D', image: '', preco: 40.00, quantidade: 0, subtotal: 0},  
-          {id: 4, nome: 'E', image: '', preco: 50.00, quantidade: 0, subtotal: 0},  
-          {id: 5, nome: 'F', image: '', preco: 60.00, quantidade: 0, subtotal: 0},  
-          {id: 6, nome: 'G', image: '', preco: 70.00, quantidade: 0, subtotal: 0},  
-      ],
-      total: 0
+        posts: [
+            {
+            "id": 0,
+            "title": "Metro City",
+            "descrisao": "Super tower",
+            "data": new Date()
+            },
+            {
+                "id": 0,
+                "title": "Metro City",
+                "descrisao": "Super tower",
+                "data": new Date()
+            },
+            {
+                "id": 0,
+                "title": "Metro City",
+                "descrisao": "Super tower",
+                "data": new Date()
+                
+            }
+        ]
     },
-    methods:{
-        getTotal: function(e){
-            this.total += e;
-        },
-        
-    }
+    // created: fetch('services/posts.json').then(function(response){
+    //             console.log(response)
+    //             this.posts = response.json();
+    //         }
+    // )
+    
   })
