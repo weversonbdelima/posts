f770aeaf2e56d1dc6d7a5bf1cbeab14e83e44846
weversonbdelima <weversonbdelima@gmail.com>
2021-03-19 17:05:30 -0300

    feat: finalização app posts

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,28 +1,90 @@
+
+
 //definição
-var postComponent = Vue.extend({
+Vue.component('post',{
     template: `
-        <div id="post"class="shadow p-3 mb-5 bg-white rounded">
+        <div id="post" class="shadow p-3 mb-5 bg-white rounded">
             <div id="card" class="" style="width: 14rem;">
-                <h3>{{title}}</h3>
+                <h3>{{titulo}}</h3>
                 </br>
                 <span>{{descrisao}}</span>
                 <div id="data">
-                    <span>{{data}}</span>
+                    {{data}}
                 </div>
+                <button class="btn btn-danger" v-on:click="deletarPost()">Delete</button>
             </div>
         </div>
     `,
     props: {
-        title: String,
+        id: Number,
+        titulo: String,
         descrisao: String,
         data: Date,
     },
+    methods:{
+        deletarPost: function(){
+            this.$emit('event-deletar-post', this.id)
+            console.log(this.id);
+        }
+    }
 })  
 
-//Registro
-Vue.component('post', postComponent);
+//Component list-post
+Vue.component('list-post',{
+    props:{
+        posts: Array
+    },
+    template:`
+    <div id="list-post">
+        <post @event-deletar-post="deletarPost" v-for="post in posts"
+            v-bind:id="post.id"
+            v-bind:titulo="post.titulo"
+            v-bind:descrisao="post.descrisao"
+            v-bind:data="post.data"
+        ></post>
+    </div>
+    `,
+    methods:{
+        deletarPost: function(e){
+            console.log('pai' + e)
+            this.$emit('event-deletar-post-avo', e);
+        }
+    }
+    
+})
 
-//Importando dados
+//Component list-post
+Vue.component('create-post',{
+    data: function(){
+        return {
+            post: {},
+        }
+    },
+    template:`
+    <div id="create-post" >
+        <h1>Criar posts</h1>
+        <label>Título</label>
+        <input placeholder="Título" v-model="post.titulo"/>
+        <label>Descrição</label>
+        <textarea placeholder="Descrição" v-model="post.descrisao" @keyup.enter="createPost"/>
+        
+    </div>
+    `,
+    methods:{
+        createPost: function(){
+            //Insere data de criação do post
+            this.post.data = new Date();
+            this.$emit('event-create-post', this.post);
+        console.log(this.post)
+        },
+        
+    }
+    
+})
+
+
+
+
 
 
 var app = new Vue({
@@ -31,25 +93,37 @@ var app = new Vue({
         posts: [
             {
             "id": 0,
-            "title": "Metro City",
+            "titulo": "Metro City",
             "descrisao": "Super tower",
             "data": new Date()
             },
             {
-                "id": 0,
-                "title": "Metro City",
+                "id": 1,
+                "titulo": "Metro City",
                 "descrisao": "Super tower",
                 "data": new Date()
             },
             {
-                "id": 0,
-                "title": "Metro City",
+                "id": 2,
+                "titulo": "Metro City",
                 "descrisao": "Super tower",
                 "data": new Date()
                 
             }
         ]
     },
+    methods:{
+        adicionarPost: function(e){
+            this.posts.push(e);       
+        },
+        deletarPost: function(e){
+            for(var i=0; i < this.posts.length; i++){
+                if(e == this.posts[i].id){
+                    this.posts.splice(i,1);
+                }
+            }
+        }
+    }
     // created: fetch('services/posts.json').then(function(response){
     //             console.log(response)
     //             this.posts = response.json();
